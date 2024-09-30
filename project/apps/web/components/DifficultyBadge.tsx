6a5230d0c9dc8121fcdc0bb0a9399e3b2c4943e9
Jeffrey <jeffreyjianyujie@gmail.com>
2024-09-30 16:45:21 +0800

    web use new enums 2

--- project/apps/web/components/DifficultyBadge.tsx
+++ project/apps/web/components/DifficultyBadge.tsx
@@ -1,5 +1,5 @@
 import { Badge } from "@/components/ui/badge";
-import { COMPLEXITY } from "@/constants/question";
+import { QuestionComplexity } from "@repo/dtos/generated/enums/questions.enums";
 import { cn } from "@/lib/utils";
 
 interface DifficultyBadgeProps {
@@ -9,11 +9,11 @@ interface DifficultyBadgeProps {
 
 const getBadgeColor = (complexity: string) => {
   switch (complexity) {
-    case COMPLEXITY.Easy:
+    case QuestionComplexity.Easy:
       return "bg-green-400 text-white hover:bg-green-400";
-    case COMPLEXITY.Medium:
+    case QuestionComplexity.Medium:
       return "bg-yellow-400 text-white hover:bg-yellow-400";
-    case COMPLEXITY.Hard:
+    case QuestionComplexity.Hard:
       return "bg-red-400 text-white hover:bg-red-400";
     default:
       return "bg-secondary text-white";
