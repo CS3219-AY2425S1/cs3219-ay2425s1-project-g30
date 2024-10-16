572f76c92b99b2a871296b3b971c5d9181c9e321
javinchua <javin.chua@gmail.com>
2024-10-16 12:35:39 +0800

    add env validation for auth-service

--- project/apps/auth-service/src/auth.module.ts
+++ project/apps/auth-service/src/auth.module.ts
@@ -5,11 +5,23 @@ import { AuthController } from 'src/adapters/controllers/auth.controller';
 import { SupabaseAuthRepository } from 'src/adapters/db/auth.supabase';
 import { AuthService } from 'src/domain/ports/auth.service';
 import { AuthRepository } from 'src/domain/ports/auth.repository';
+import { envSchema } from './config/env';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate: (config) => {
+        const parsedEnv = envSchema.safeParse(config);
+        if (!parsedEnv.success) {
+          console.error(
+            '❌ Invalid environment variables:',
+            parsedEnv.error.format(),
+          );
+          throw new Error('Invalid environment variables');
+        }
+        return parsedEnv.data;
+      },
     }),
   ],
   controllers: [AuthController],
--- project/apps/auth-service/src/main.ts
+++ project/apps/auth-service/src/main.ts
@@ -1,24 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+
+@Module({
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+class BootstrapConfigModule {}
 
 async function bootstrap() {
-  const host =
-    process.env.NODE_ENV === 'development'
-      ? 'localhost'
-      : process.env.AUTH_SERVICE_HOST || 'localhost';
+  const appContext = await NestFactory.createApplicationContext(
+    BootstrapConfigModule,
+  );
+  const configService = appContext.get(ConfigService);
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+  const AUTH_SERVICE_HOST = configService.get<string>('AUTH_SERVICE_HOST');
+  appContext.close();
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AuthModule,
     {
       transport: Transport.TCP,
       options: {
-        host: host,
+        host: NODE_ENV === 'development' ? 'localhost' : AUTH_SERVICE_HOST,
         port: 3003,
       },
     },
   );
   await app.listen();
-  console.log(`Auth Service is listening on ${host}:3003`);
+  console.log(`Auth Service is listening on ${AUTH_SERVICE_HOST}:3003`);
 }
 bootstrap();
