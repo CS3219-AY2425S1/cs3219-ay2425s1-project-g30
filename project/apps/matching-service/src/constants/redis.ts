795803f601f86d1fa7c05ec004125f9a811518d8
firwer <firwer@gmail.com>
2024-10-17 13:05:02 +0800

    fix: magic number

--- project/apps/matching-service/src/constants/redis.ts
+++ project/apps/matching-service/src/constants/redis.ts
@@ -2,8 +2,6 @@ import { CacheModuleAsyncOptions } from "@nestjs/cache-manager";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
 export const REDIS_CLIENT = 'REDIS_CLIENT';
-
-export const MATCH_WAITING_KEY = 'match-waiting';
 export const MATCH_CANCELLED_KEY = 'match-cancelled';
 export const SOCKET_USER_KEY = 'socket-user';
 export const USER_SOCKET_KEY = 'user-socket';
