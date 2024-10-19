2fd14a24b60c5aa638d181da9b75c42fa5b17d82
firwer <firwer@gmail.com>
2024-10-19 18:31:10 +0800

    feat: match start_time and end_time fields

--- project/packages/dtos/src/match.ts
+++ project/packages/dtos/src/match.ts
@@ -20,6 +20,8 @@ export const matchDataSchema = matchCriteriaSchema.extend({
   user2_id: z.string().uuid(),
   id: z.string().uuid(),
   question_id: z.string().uuid(),
+  start_time: z.number().int(),
+  end_time: z.number().int().nullable(),
 });
 
 export const matchRequestMsgSchema = matchCriteriaSchema.extend({
