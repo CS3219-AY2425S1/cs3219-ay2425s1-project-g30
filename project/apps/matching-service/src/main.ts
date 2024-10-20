96577115ad8001344fae4e8b4c00b08d1237bd77
Jeffrey <jeffreyjianyujie@gmail.com>
2024-10-21 03:55:24 +0800

    hotfix: matching-service not starting on docker

--- project/apps/matching-service/src/main.ts
+++ project/apps/matching-service/src/main.ts
@@ -1,24 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { EnvService } from './env/env.service';
+// import { EnvService } from './env/env.service';
 import { MatchingModule } from './matching.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MatchingModule);
-  const envService = app.get(EnvService);
-  const NODE_ENV = envService.get('NODE_ENV');
-  const MATCHING_SERVICE_HOST = envService.get('MATCHING_SERVICE_HOST');
+  // const appContext = await NestFactory.createApplicationContext(MatchingModule);
+  // const envService = appContext.get(EnvService);
+  // const NODE_ENV = envService.get('NODE_ENV');
+  // const MATCHING_SERVICE_HOST = envService.get('MATCHING_SERVICE_HOST');
+  // appContext.close();
 
-  const host = NODE_ENV === 'development' ? 'localhost' : MATCHING_SERVICE_HOST;
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      host: host,
-      port: 3004,
+  // Reverting back to use process.env for milestone D4
+
+  const host =
+    process.env.NODE_ENV === 'development'
+      ? 'localhost'
+      : process.env.MATCHING_SERVICE_HOST;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MatchingModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: host,
+        port: 3004,
+      },
     },
-  });
-  await app.startAllMicroservices();
-  await app.listen(3004);
+  );
+
+  await app.listen();
   console.log(`Matching Service is listening on ${host}:3004`);
 }
 bootstrap();
