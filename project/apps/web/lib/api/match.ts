cd3e723b2929f1a411ec4cf1ffe9fef0b6a7bdc7
Jonathan Yeo <jonathan.yeo@bytedance.com>
2024-10-20 16:12:14 +0800

    fix: match data return type in api

--- project/apps/web/lib/api/match.ts
+++ project/apps/web/lib/api/match.ts
@@ -1,19 +1,19 @@
-import { MatchDataDto, MatchRequestMsgDto } from '@repo/dtos/match';
+import { MatchRequestMsgDto } from '@repo/dtos/match';
 
 import { apiCall } from '@/lib/api/apiClient';
 
 export const createMatch = async (
   queryParams: MatchRequestMsgDto,
-): Promise<MatchDataDto> => {
-  return await apiCall('post', '/matches', null, queryParams);
+): Promise<any> => {
+  return await apiCall('post', '/matches', queryParams);
 };
 
-export const getMatchById = async (id: string): Promise<MatchDataDto> => {
+export const getMatchById = async (id: string): Promise<any> => {
   return await apiCall('get', `/matches/${id}`);
 };
 
 export const getMatchesByUserId = async (
   id: string,
-): Promise<MatchDataDto[]> => {
+): Promise<any[]> => {
   return await apiCall('get', `/matches/user/${id}`);
 };
