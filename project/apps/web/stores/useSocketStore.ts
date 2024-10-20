8ceebe331518164a171e5c62476a6960279e668f
Jonathan Yeo <jonathan.yeo@bytedance.com>
2024-10-20 17:42:04 +0800

    feat: add toast for expiry and invalid match

--- project/apps/web/stores/useSocketStore.ts
+++ project/apps/web/stores/useSocketStore.ts
@@ -1,8 +1,8 @@
 import { MatchDataDto } from '@repo/dtos/match';
 import { io, Socket } from 'socket.io-client';
 import { create } from 'zustand';
-
-import { env } from '@/env.mjs';
+import { env } from '@/env.mjs'; 
+import { Router } from 'next/router';
 
 const SOCKET_SERVER_URL = env.NEXT_PUBLIC_MATCH_SOCKET_URL;
 
@@ -10,11 +10,13 @@ interface SocketState {
   socket: Socket | null;
   isConnected: boolean;
   isSearching: boolean;
+  toastMessage: { variant: "default" | "success" | "error" | "destructive" | null | undefined, title: string, description: string } | null;
   connectionError: string | null;
   connect: () => void;
   startSearch: () => void;
   stopSearch: () => void;
   disconnect: () => void;
+  clearToastMessage: () => void;
 }
 
 const useSocketStore = create<SocketState>((set, get) => ({
@@ -22,7 +24,8 @@ const useSocketStore = create<SocketState>((set, get) => ({
   isConnected: false,
   isSearching: false,
   connectionError: null,
-  connect: () => {
+  toastMessage: null,
+  connect: (router) => {
     if (get().socket) return; // Prevent multiple connections
 
     const socket = io(SOCKET_SERVER_URL, {
@@ -48,16 +51,38 @@ const useSocketStore = create<SocketState>((set, get) => ({
     socket.on('match_found', (message: MatchDataDto) => {
       console.log('Match found:', message);
       get().stopSearch();
+
+      set({
+        toastMessage: {
+          variant: 'success',
+          title: 'Match Found',
+          description: 'Your match was successful.',
+        },
+      });
     });
 
     socket.on('match_invalid', (message: string) => {
       console.log('Match invalid:', message);
       get().stopSearch();
+      set({
+        toastMessage: {
+          variant: 'error',
+          title: 'Match Invalid',
+          description: 'Your match was invalid.',
+        },
+      });
     });
 
     socket.on('match_request_expired', (message: string) => {
       console.log('Match request expired:', message);
       get().stopSearch();
+      set({
+        toastMessage: {
+          variant: 'error',
+          title: 'Match Expired',
+          description: 'Your match request exceeds 2 minutes and has expired. Please try again.',
+        },
+      });
     });
 
     set({ socket });
@@ -83,6 +108,7 @@ const useSocketStore = create<SocketState>((set, get) => ({
       });
     }
   },
+  clearToastMessage: () => set({ toastMessage: null }),
 }));
 
 export default useSocketStore;
