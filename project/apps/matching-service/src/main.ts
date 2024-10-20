7576419f45a2f15d4e21308df360b404de8509bd
javinchua <javin.chua@gmail.com>
2024-10-20 22:45:58 +0800

    fix appcontext causing nestjs to start the application twice

--- project/apps/matching-service/src/main.ts
+++ project/apps/matching-service/src/main.ts
@@ -4,13 +4,16 @@ import { EnvService } from './env/env.service';
 import { MatchingModule } from './matching.module';
 
 async function bootstrap() {
-  const appContext = await NestFactory.createApplicationContext(MatchingModule);
-  const envService = appContext.get(EnvService);
-  const NODE_ENV = envService.get('NODE_ENV');
-  const MATCHING_SERVICE_HOST = envService.get('MATCHING_SERVICE_HOST');
-  appContext.close();
+  // const appContext = await NestFactory.createApplicationContext(MatchingModule);
+  // const envService = appContext.get(EnvService);
+  // const NODE_ENV = envService.get('NODE_ENV');
+  // const MATCHING_SERVICE_HOST = envService.get('MATCHING_SERVICE_HOST');
+  // appContext.close();
 
-  const host = NODE_ENV === 'development' ? 'localhost' : MATCHING_SERVICE_HOST;
+  const host =
+    process.env.NODE_ENV === 'development'
+      ? 'localhost'
+      : process.env.MATCHING_SERVICE_HOST;
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     MatchingModule,
     {
