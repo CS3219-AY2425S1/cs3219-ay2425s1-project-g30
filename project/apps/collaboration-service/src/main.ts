fca72a11c0d786548e0f634f1525955c0ca93ba4
Jeffrey <jeffreyjianyujie@gmail.com>
2024-10-23 15:36:48 +0800

    clean base collaboration-service/ dir

--- project/apps/collaboration-service/src/main.ts
+++ project/apps/collaboration-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+import { CollaborationModule } from './collaboration.module';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // will use envService after integrating with the env module
+  const host =
+    process.env.NODE_ENV === 'development'
+      ? 'localhost'
+      : process.env.COLLABORATION_SERVICE_HOST;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CollaborationModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: host,
+        port: 3005,
+      },
+    },
+  );
+
+  await app.listen();
+  console.log(`Collaboration Service is listening on ${host}:3005`);
 }
 bootstrap();
