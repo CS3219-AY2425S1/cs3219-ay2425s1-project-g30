b4a40d1f4a48a983ed8b6f4dec956c5041d507b3
Gallen <90502827+gongg21@users.noreply.github.com>
2024-11-06 01:03:05 +0800

    fix unauthorised access redirect

--- project/apps/web/app/manage-users/page.tsx
+++ project/apps/web/app/manage-users/page.tsx
@@ -6,9 +6,15 @@ import { ActionModals } from '@/components/manage-users/ActionModals';
 import ManageUsersSkeleton from '@/components/manage-users/ManageUsersSkeleton';
 import { UsersTable } from '@/components/manage-users/users-table/UsersTable';
 import { useManageUsersStore } from '@/stores/useManageUsersStore';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { notFound } from 'next/navigation';
 
 const ManageUsersRepositoryContent = () => {
+  const user = useAuthStore.use.user();
   const selectedUser = useManageUsersStore.use.selectedUser();
+  if (user && user.role !== 'Admin') {
+    return notFound();
+  }
 
   return (
     <div className="container p-6 mx-auto">
