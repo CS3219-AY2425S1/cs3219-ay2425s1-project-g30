2924264f7c3a9da71bce967bf31ccfc33d3ab1d9
Gallen <90502827+gongg21@users.noreply.github.com>
2024-11-06 15:31:17 +0800

    fix debounce + number of entries

--- project/apps/web/components/manage-users/users-table/UsersTableRowActions.tsx
+++ project/apps/web/components/manage-users/users-table/UsersTableRowActions.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { useAuthStore } from '@/stores/useAuthStore';
 import { useManageUsersStore } from '@/stores/useManageUsersStore';
 
 interface UsersTableRowActionsProps<TData> {
@@ -21,6 +22,7 @@ interface UsersTableRowActionsProps<TData> {
 export function UsersTableRowActions<TData>({
   row,
 }: UsersTableRowActionsProps<TData>) {
+  const adminUser = useAuthStore.use.user();
   const setUpdateModalOpen = useManageUsersStore.use.setUpdateModalOpen();
   const setDeleteModalOpen = useManageUsersStore.use.setDeleteModalOpen();
   const setSelectedUser = useManageUsersStore.use.setSelectedUser();
@@ -38,25 +40,27 @@ export function UsersTableRowActions<TData>({
 
   return (
     <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-          >
-            <DotsHorizontalIcon className="w-4 h-4" />
-            <span className="sr-only">Open menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-[160px]">
-          <DropdownMenuItem className="gap-2" onSelect={handleOpenEdit}>
-            <Pencil className="w-4 h-4" /> Update
-          </DropdownMenuItem>
-          <DropdownMenuItem className="gap-2" onSelect={handleOpenDelete}>
-            <Trash2 className="w-4 h-4" /> Delete
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      {adminUser && adminUser.id !== user.id && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+            >
+              <DotsHorizontalIcon className="w-4 h-4" />
+              <span className="sr-only">Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-[160px]">
+            <DropdownMenuItem className="gap-2" onSelect={handleOpenEdit}>
+              <Pencil className="w-4 h-4" /> Update
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-2" onSelect={handleOpenDelete}>
+              <Trash2 className="w-4 h-4" /> Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </>
   );
 }
