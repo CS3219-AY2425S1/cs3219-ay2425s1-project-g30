c867c858ea0c01ad42d758ec6891a2c8eb254860
jonyeokj <joyekajie@gmail.com>
2024-11-07 15:32:22 +0800

    fix: history local time instead of utc

--- project/apps/web/components/ErrorFallback.tsx
+++ project/apps/web/components/ErrorFallback.tsx
@@ -4,9 +4,9 @@ import { Button } from './ui/button';
 
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full gap-2">
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-2">
       <h2 className="font-semibold">Oops! Something went wrong.</h2>
-      {error?.message ? <div>{error.message}</div> : null}
+      {error?.message ? <div>Error: {error.message}</div> : null}
       <div className="mb-2">Please try again.</div>
       <Button onClick={resetErrorBoundary}>Retry</Button>
     </div>
